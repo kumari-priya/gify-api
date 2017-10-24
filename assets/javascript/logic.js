8962cfb15e84047b6924f0f3a1e0b1840f477a3f
Kumari Priya <priya@goel.pw>
2017-10-23 21:10:32 -0700

    update js

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -3,20 +3,20 @@ var topics = ["happy", "sad", "mad", "excited", "grumpy", "cry", "smile", "alone
 // API key
 var apiKey = "K9UMRtFI3MtDZVcIXpmUykUj5eYOlI2k";
 // API image limit
-var limit = "20";
-
+var limit = 20;
 
 // create buttons from the array
 function renderButtons() {
     $("#gifs-view").empty();
     for (var i = 0; i < topics.length; i++) {
         var newButton = $("<button>");
-        newButton.addClass("btn btn-primary gif");
+        newButton.addClass("btn btn-info gif");
         newButton.attr("data-name", topics[i]);
         newButton.text(topics[i]);
         $("#gifs-view").append(newButton);
     }
 }
+
 // Add gif button
 $("#add-gif").on("click", function(event) {
     event.preventDefault();
@@ -26,6 +26,7 @@ $("#add-gif").on("click", function(event) {
     $("#gif-input").val("");
 });
 
+// Display gifs when the dynamically rendered button is clicked
 function displayGifs() {
     var name = $(this).attr("data-name");
     // API url
@@ -56,6 +57,7 @@ function displayGifs() {
     });
 }
 
+// Play-pause gif function
 function playGifs() {
     var image = $(this);
     var state = image.attr("data-state");
