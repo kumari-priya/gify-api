8e91e7888b7d6dea2f4975b8acd3a22a4418211a
Kumari Priya <priya@goel.pw>
2017-10-23 20:24:13 -0700

    add assets

---
+++ assets/javascript/logic.js
@@ -0,0 +1,69 @@
+var topics = ["happy","sad","mad","excited","grumpy","cry","smile","alone","awake","blah","bored","calm","chilled","cold","confused","curious","guilty","hopeful","lazy","moody","nerdy","okay"];
+
+function displayGifs() {
+    var name = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        name + "&api_key=dc6zaTOxFJmzC&limit=20";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response) {
+        console.log(response);
+        var results = response.data;
+        for (var i = 0; i < results.length; i++) {
+            var resultDiv = $("<div>");
+            resultDiv.addClass("panel panel-default img-box");
+            var p = $("<p>");
+            p.text("Rating : "+results[i].rating);
+            var resultImage = $("<img>");
+            resultImage.attr("src", results[i].images.fixed_height_still.url);
+            resultImage.attr("data-still", results[i].images.fixed_height_still.url);
+            resultImage.attr("data-animate", results[i].images.fixed_height.url);
+            resultImage.attr("data-state", "still");
+            resultImage.addClass("play");
+            resultDiv.append(p);
+            resultDiv.append(resultImage);
+            $("#gif-result").prepend(resultDiv);
+        }
+    });
+}
+
+
+function renderButtons() {
+    $("#gifs-view").empty();
+    for (var i = 0; i < topics.length; i++) {
+        var newButton = $("<button>");
+        newButton.addClass("btn btn-primary gif");
+        newButton.attr("data-name", topics[i]);
+        newButton.text(topics[i]);
+        $("#gifs-view").append(newButton);
+    }
+}
+
+$("#add-gif").on("click", function(event) {
+    event.preventDefault();
+    var gif = $("#gif-input").val().trim();
+    topics.push(gif);
+    renderButtons();
+});
+
+function playGifs() {
+    var image = $(this);
+    var state = image.attr("data-state");
+
+    if (state === "still") {
+        var animate = image.attr("data-animate");
+        image.attr("src", animate);
+        image.attr("data-state", "animate");
+    } else {
+        var still = image.attr("data-still");
+        image.attr("src", still);
+        image.attr("data-state", "still");
+    }
+}
+
+renderButtons();
+
+$(document).on("click", ".gif", displayGifs);
+
+$(document).on("click", ".play", playGifs);
